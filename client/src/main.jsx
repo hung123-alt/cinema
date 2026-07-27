4f48107d993a46ed020eb6f3370fc12137ae9a0b
hung123-alt <loct63636@gmail.com>
2026-07-27 06:16:48 +0000

    Add Vercel Speed Insights

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from './context/ThemeContext'
 import { ToastProvider } from './context/ToastContext'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider>
       <ToastProvider>
         <App />
+        <SpeedInsights />
       </ToastProvider>
     </ThemeProvider>
   </StrictMode>,
